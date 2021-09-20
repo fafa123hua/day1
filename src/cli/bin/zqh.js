1f9e34b7a3f350452936b563b09ebc9f6cf677d1
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-09-20 22:47:48 +0800

    fix:cli

--- src/cli/bin/zqh.js
+++ src/cli/bin/zqh.js
@@ -1,3 +1,10 @@
 #! /usr/bin/env node
 
-console.log(12312321)
+const program = require('commander')
+program.version(require('../package').version)
+program.command('init <name>')
+    .description('init project')
+    .action(name => {
+        console.log('init' + name)
+    })
+program.parse(process.argv)
