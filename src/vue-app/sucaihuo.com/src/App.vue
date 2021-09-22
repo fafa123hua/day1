6a703b46a62c111d50864135628cf43bedca45a2
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-09-22 19:19:28 +0800

    feat:vue-app

---
+++ src/vue-app/sucaihuo.com/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div>组件1</div>
+    <div>组件2</div>
+    <div>组件3</div>
+    <div>组件4</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
